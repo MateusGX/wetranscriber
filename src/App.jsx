78e4bf380db549785cd23e561bfab515d47d7f92
BuildTools <unconfigured@null.spigotmc.org>
2023-11-01 00:50:15 -0300

    Revert "use finalTranscript"
    This reverts commit e90c541a2e11ec9990042cadb4750f886ba89417.

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { Download, RotateCcw } from "lucide-react";
 
 function App() {
   const {
-    finalTranscript,
+    transcript,
     listening,
     resetTranscript,
     browserSupportsSpeechRecognition,
@@ -30,7 +30,7 @@ function App() {
   return (
     <div className="main">
       <div className="transcriptContainer">
-        <p className="transcript">{finalTranscript || ". . ."}</p>
+        <p className="transcript">{transcript || ". . ."}</p>
       </div>
 
       <div className="buttons">
@@ -46,7 +46,7 @@ function App() {
         >
           {listening ? "STOP LISTENING" : "START LISTENING"}
         </button>
-        <button onClick={() => exportTranscript(finalTranscript)}>
+        <button onClick={() => exportTranscript(transcript)}>
           <Download />
         </button>
       </div>
